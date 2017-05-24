471d1aabf19a52c22711ab625cc246c6b9b66106
Ubuntu <ubuntu@ip-172-31-16-62.us-west-2.compute.internal>
2017-05-24 19:04:29 +0000

    begin nn elaboration

--- fraud/nn.py
+++ fraud/nn.py
@@ -67,7 +67,7 @@ def main(_):
   # Test trained model
   correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-  print(sess.run(accuracy, feed_dict={x: mnist.test.images,
+  print('accuracy: %s'%sess.run(accuracy, feed_dict={x: mnist.test.images,
                                       y_: mnist.test.labels}))
 
 if __name__ == '__main__':
