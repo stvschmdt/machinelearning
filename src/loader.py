7e8ca58f8b1accc5a6a4008768e9c527fbd42ec5
Ubuntu <ubuntu@ip-172-31-16-62.us-west-2.compute.internal>
2017-06-01 13:14:26 +0000

    classify loader.py

--- src/loader.py
+++ src/loader.py
@@ -2,25 +2,31 @@ import os
 from scipy import ndimage, misc
 import re
 import numpy as np
+from logger import Logging
 
-def reader():
-    d_images = []
-    c_images = []
-    for root, dirnames, filenames in os.walk("../images/train/"):
-        for filename in filenames:
-            if re.search("\.(jpg|jpeg|png|bmp|tiff)$", filename):
-                filepath = os.path.join(root, filename)
-                image = ndimage.imread(filepath, mode="RGB")
-                image_resized = misc.imresize(image, (28, 28))
-                if re.search("dog", filename):
-                    d_images.append(image_resized)
-                else:
-                    c_images.append(image_resized)
-                if (len(d_images) + len(c_images))% 1000 == 0:
-                    print 'read %s dog and %s cat images'%(len(d_images), len(c_images))
+class Reader():
+    def __init__(self):
+        self.logger = Logging()
 
-    print 'read %s dog and %s cat images'%(len(d_images), len(c_images))
-    return np.array(d_images), np.array(c_images)
+    def read_images(self, fname, sep):
+        d_images = []
+        c_images = []
+        for root, dirnames, filenames in os.walk(fname):
+            for filename in filenames:
+                if re.search("\.(jpg|jpeg|png|bmp|tiff)$", filename):
+                    filepath = os.path.join(root, filename)
+                    image = ndimage.imread(filepath, mode="RGB")
+                    image_resized = misc.imresize(image, (28, 28))
+                    if re.search(sep, filename):
+                        d_images.append(image_resized)
+                    else:
+                        c_images.append(image_resized)
+                    if (len(d_images) + len(c_images))% 1000 == 0:
+                        self.logger.info('read %s/%s %s/not %s images'%(len(d_images), len(c_images), sep, sep))
+
+        self.logger.info('read %s dog and %s cat images'%(len(d_images), len(c_images)))
+        return np.array(d_images), np.array(c_images)
 
 if __name__ == '__main__':
-    reader()
+    r = Reader()
+    arr = r.read_images('../../store/images_data/train', 'dog')
