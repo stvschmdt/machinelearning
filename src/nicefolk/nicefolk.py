18b32ccfedaab53af87c219d342ca46f0e5176dc
Ubuntu <ubuntu@ip-172-31-16-62.us-west-2.compute.internal>
2017-07-28 23:47:47 +0000

    cleanup, comments, argparse

--- src/nicefolk/nicefolk.py
+++ src/nicefolk/nicefolk.py
@@ -99,17 +99,21 @@ def one_hot(x):
     y[x] = 1.0
     return y
 
+#goodfellow et al attack x'=x+epsilon*sgn(gradient)
 def goodfellow_mod(x, grad, epsilon=0.05):
     xprime =  x + np.sign(grad)*epsilon
+    #dumb way to keep within 0,1, empirical testing will determine keeping
     xprime[ xprime < 0.0] = 0
     xprime[ xprime > 1.0] = 0
     return xprime
 
+#convert a list of tuples into nparray
 def image_list_to_np(l_image, idx):
     vals = [ x[idx] for x in l_image ]
     vals = np.array(vals)
     return vals
 
+#split data arrays with images and labels into train/test set
 def split_train_data(xarr, yarr, n):
   train_images = np.array(xarr[:-n])
   train_labels = np.array(yarr[:-n])
@@ -120,6 +124,7 @@ def split_train_data(xarr, yarr, n):
 def main(_):
   # import data
   mdl = BlackBox(FLAGS)
+  # NOTE this will work with format: mdl.oracle = (image, pred_val, true_val)
   logger.info('obtained black box training data')
   mnist = mdl.oracle
   # translate into tensorflow style nparrays
@@ -143,27 +148,32 @@ def main(_):
   # define loss function -> cross entropy for now with softmax
   cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
   # train step, 1e-4 is default, best to use -2/-3 depending on time
-  train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
+  train_step = tf.train.AdamOptimizer(FLAGS.opt).minimize(cross_entropy)
   # define correct prediction vectore and accuracy comparison
   correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
  
   # split training and test data into nparrays
-  train_images, train_labels, test_images, test_labels =split_train_data(x_vals, y_vals, 200)
+  train_images, train_labels, test_images, test_labels =split_train_data(x_vals, y_vals, FLAG.split)
   logger.info('training sets: %s test sets: %s ... %s %s'.format(len(train_images),len(test_images), train_images.shape, train_labels.shape))
   cnn_saver = tf.train.Saver()
-  batch_size = 200
   logger.info('starting adversarvial model training')
+  #begin training with session
   with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    for i in range(20):
+    # TODO create batching loop
+    for i in range(FLAGS.iters):
+      #sanity check on accuracy should be going down -> necessary but not sufficient
       if i % 100 == 0:
         train_accuracy = accuracy.eval(feed_dict={x: train_images, y_: train_labels, keep_prob: 1.0})
         logger.info('step %d, training accuracy %g' % (i, train_accuracy))
+      #update rule - softmax vector obtained for checking
       trainer, softmax = sess.run([train_step, cross_entropy],feed_dict={x: train_images, y_: train_labels, keep_prob: 0.5})
     logger.info('adversarial model has been trained')
+    # snag the gradient vector wrt X inputs
     grads = tf.gradients(cross_entropy, [x])
-    gs = sess.run(grads, feed_dict={x:test_images, y_:test_labels, keep_prob: 1.0})
+    jacobian = sess.run(grads, feed_dict={x:test_images, y_:test_labels, keep_prob: 1.0})
+    #use test data as input for perturbations
     test_ = tf.argmax(y_,1)
     test_vals = test_.eval(feed_dict={y_:test_labels})
     pred_ = tf.argmax(y_conv,1)
@@ -171,28 +181,35 @@ def main(_):
     true_pred = [ (pxl, p) for pxl, p, r in zip(test_images, pred_vals, test_vals) if p==r ]
     logger.info('true positive test exemplars: %s'.format(len(true_pred)))
     logger.results('adversary accuracy: %g' % (accuracy.eval(feed_dict={x: test_images, y_: test_labels, keep_prob: 1.0})))
-    xprime = []
-    yprime = []
-    for epsilon in np.linspace(0.05,.35,num=10):
+    #setup the goodfellow attack iterations
+    for epsilon in np.linspace(0.05,.35,num=FLAGS.augments):
         xprime = []
         yprime = []
         for idx,pos in enumerate(true_pred):
-          xp = goodfellow_mod(np.array(pos[0]), gs[0][idx], epsilon)
+          xp = goodfellow_mod(np.array(pos[0]), jacobian[0][idx], epsilon)
           prime_label = one_hot(int(pos[1]))
           xprime.append(xp)
           yprime.append(prime_label)
-
+        #can do this each iteration - or as a whole...at this point timing doesnt matter, but will
         xprime = np.array(xprime)
         yprime = np.array(yprime)
         yprime = yprime.reshape((len(yprime), 10))
         logger.results('adversary accuracy: %g %f' % (accuracy.eval(feed_dict={x: xprime, y_: yprime, keep_prob: 1.0}), epsilon))
 
+    # save model to file
     cnn_saver_path = cnn_saver.save(sess, 'cnn_saver.ckpt')
 
 
 if __name__ == '__main__':
+# args: batch_size, graddescent number, training iters, epsilon trial space
   parser = argparse.ArgumentParser()
-  parser.add_argument('--data_dir', type=str,default='/tmp/tensorflow/mnist/input_data',help='Directory for storing input data')
+  parser.add_argument('--data_dir', type=str,default='/tmp/tensorflow/mnist/input_data',help='directory for storing input data')
+  parser.add_argument('--batch_size', type=int,default=100,help='batch size for stochastic gradient descent')
+  parser.add_argument('--optimize', type=float,default=1e-3,help='threshold for adam optimizer')
+  parser.add_argument('--iters', type=int,default=500,help='cnn training epochs')
+  parser.add_argument('--augments', type=int,default=10,help='number of attack augmentations to sample for epsilon on inputs')
+  parser.add_argument('--split', type=float,default=200,help='train test set percent split')
+  parser.add_argument('--fsplit', type=float,default=None,help='train test set percent split')
   FLAGS, unparsed = parser.parse_known_args()
   tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
 
