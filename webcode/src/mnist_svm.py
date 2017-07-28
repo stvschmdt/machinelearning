41f65dfe78d29386bddb5699b4fbdffef4d86a84
Ubuntu <ubuntu@ip-172-31-16-62.us-west-2.compute.internal>
2017-07-28 17:28:27 +0000

    edit

--- webcode/src/mnist_svm.py
+++ webcode/src/mnist_svm.py
@@ -5,16 +5,13 @@ mnist_svm
 A classifier program for recognizing handwritten digits from the MNIST
 data set, using an SVM classifier."""
 
-#### Libraries
-# My libraries
 import mnist_loader 
-
-# Third-party libraries
 from sklearn import svm
+from sklearn.externals import joblib
 
 def svm_baseline():
     training_data, validation_data, test_data = mnist_loader.load_data()
-    print training_data[0][0].shape, training_data[0][0].shape
+    #print training_data[0][0].shape, training_data[0][0].shape
     # train
     clf = svm.SVC()
     clf.fit(training_data[0], training_data[1])
@@ -23,6 +20,7 @@ def svm_baseline():
     num_correct = sum(int(a == y) for a, y in zip(predictions, test_data[1]))
     print "Baseline classifier using an SVM."
     print "%s of %s values correct." % (num_correct, len(test_data[1]))
+    joblib.dump(clf, '/home/ubuntu/machinelearning/src/nicefolk/simple_svm.pkl')
 
 if __name__ == "__main__":
     svm_baseline()
